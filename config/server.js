04e924b2315ce38194da0340d43d399bee332d86
Mohsin278 <mohsinmanzoor06@gmail.com>
2025-02-12 11:41:49 +0530

    Update package-lock.json after fixing npm errors

---
+++ config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'yourAdminJWTSecret'),
+    },
+  },
+  keys: env.array('APP_KEYS', ['abc123xyz456', 'def789uvw012']),
+});
