42eda36246c4efb429cbb846c1016cb1ca6d3905
Mohsin278 <mohsinmanzoor06@gmail.com>
2025-02-12 12:31:14 +0530

    Fixed Server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,12 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS', ['abc123xyz456', 'def789uvw012']), // Moved inside `app`
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'yourAdminJWTSecret'),
     },
   },
-  keys: env.array('APP_KEYS', ['abc123xyz456', 'def789uvw012']),
 });
